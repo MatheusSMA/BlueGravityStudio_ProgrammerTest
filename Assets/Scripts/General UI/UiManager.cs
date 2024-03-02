3f468c10cbfa33a902c0d819b3017ad442db559d
MatheusSMA <matheusjut@gmail.com>
2024-03-01 23:34:46 -0300

    UI manager

--- Assets/Scripts/General UI/UiManager.cs
+++ Assets/Scripts/General UI/UiManager.cs
@@ -1,6 +1,34 @@
+using System;
 using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
+    public event Action OnPlayerInventoryOpen;
+    public event Action OnPlayerInventoryClose;
+    public event Action OnShopInventoryOpen;
+    public event Action OnShopInventoryClose;
 
+    private bool _inventoryIsOpen;
+    public void PlayerInventoryOpen()
+    {
+        OnPlayerInventoryOpen?.Invoke();
+        _inventoryIsOpen = true;
+    }
+
+    public void PlayerInventoryClose()
+    {
+        OnPlayerInventoryClose?.Invoke();
+        _inventoryIsOpen = false;
+    }
+    public void ShopInventoryOpen()
+    {
+        OnShopInventoryOpen?.Invoke();
+        _inventoryIsOpen = true;
+    }
+
+    public void ShopInventoryClose()
+    {
+        OnShopInventoryClose?.Invoke();
+        _inventoryIsOpen = false;
+    }
 }

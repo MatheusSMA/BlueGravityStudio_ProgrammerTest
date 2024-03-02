8e4c667417e6dc8997e7651966c7fb0486ad532b
MatheusSMA <matheusjut@gmail.com>
2024-03-01 23:16:13 -0300

    Interact and inventory keys on playerconfig

--- Assets/Scripts/Character/Player/PlayerConfig.cs
+++ Assets/Scripts/Character/Player/PlayerConfig.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public static class PlayerConfig
 {
+    public static KeyCode INTERACT_KEY = KeyCode.E;
+    public static KeyCode INVENTORY_KEY = KeyCode.Q;
     public static string WALKING_ANIMATION_KEY = "walking";
 }
